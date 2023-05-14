ace11e925723c76a4ea373bc4c36dc7730c18f1e
NextWork123 <nextworks@protonmail.com>
2023-05-14 14:14:06 +0200

    Optimize again the entire code Removed the console.foregroundcolor and console.writeline
    - Reworked the entire logic and added more browser supported Chrome,Firefox,Edge,Thorium
    - Reworked too the logic for Discord
    - Improved ram usage at start was 5.7MB now is 0.7MB
    - Improved too the cpu usage at start was variable at 1%  now is 0.3% (i put Thread Sleep at 5 Seconds i think is much good)

--- DiscordRamLimiter/Program.cs
+++ DiscordRamLimiter/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Management;
 using System.Runtime.InteropServices;
 using System.Threading;
 
@@ -22,100 +21,37 @@ namespace DiscordRamLimiter
         const int SW_HIDE = 0;
         const int SW_SHOW = 5;
 
-        public static int GetDiscord()
-        {
-            Process[] discordProcesses = Process.GetProcessesByName("Discord");
-            if (discordProcesses.Length > 0)
-            {
-                Process discord = discordProcesses.OrderByDescending(p => p.WorkingSet64).FirstOrDefault();
-                return discord.Id;
-            }
-            return -1;
-        }
-
-        public static int GetThorium()
-        {
-            Process[] thoriumProcesses = Process.GetProcessesByName("Thorium");
-            if (thoriumProcesses.Length > 0)
-            {
-                Process thorium = thoriumProcesses.OrderByDescending(p => p.WorkingSet64).FirstOrDefault();
-                return thorium.Id;
-            }
-            return -1;
-        }
-
         static void RamLimiter(int min, int max)
         {
             var handle = GetConsoleWindow();
             ShowWindow(handle, SW_HIDE);
 
-            ManagementObjectSearcher wmiObject = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+            Process[] processes = Process.GetProcesses();
 
             while (true)
             {
-                int discordId = GetDiscord();
-                int thoriumId = GetThorium();
-
-                if (discordId != -1)
+                foreach (Process process in processes)
                 {
-                    GC.Collect(); // Force garbage collection
-                    GC.WaitForPendingFinalizers(); // Wait for all finalizers to complete before continuing.
+                    string processNameLower = process.ProcessName.ToLower();
 
-                    if (Environment.OSVersion.Platform == PlatformID.Win32NT) // Check OS version platform
+                    if (processNameLower.Contains("discord") || processNameLower.Contains("chrome") || processNameLower.Contains("firefox") || processNameLower.Contains("edge") || processNameLower.Contains("thorium"))
                     {
-                        SetProcessWorkingSetSize(Process.GetProcessById(discordId).Handle, min, max);
+                        GC.Collect(); // Force garbage collection
+                        GC.WaitForPendingFinalizers(); // Wait for all finalizers to complete before continuing.
+
+                        if (Environment.OSVersion.Platform == PlatformID.Win32NT) // Check OS version platform
+                        {
+                            SetProcessWorkingSetSize(process.Handle, min, max);
+                        }
                     }
-
-                    var memoryValues = wmiObject.Get().Cast<ManagementObject>().Select(mo => new
-                    {
-                        FreePhysicalMemory = Double.Parse(mo["FreePhysicalMemory"].ToString()),
-                        TotalVisibleMemorySize = Double.Parse(mo["TotalVisibleMemorySize"].ToString())
-                    }).FirstOrDefault();
-
-                    if (memoryValues != null)
-                    {
-                        var percent = ((memoryValues.TotalVisibleMemorySize - memoryValues.FreePhysicalMemory) / memoryValues.TotalVisibleMemorySize) * 100;
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.WriteLine("Your current memory usage (Discord): {0}", percent);
-                        Console.ResetColor();
-                    }
-                    Thread.Sleep(600);
                 }
 
-                if (thoriumId != -1)
-                {
-                    GC.Collect(); // Force garbage collection
-                    GC.WaitForPendingFinalizers(); // Wait for all finalizers to complete before continuing.
-
-                    if (Environment.OSVersion.Platform == PlatformID.Win32NT) // Check OS version platform
-                    {
-                        SetProcessWorkingSetSize(Process.GetProcessById(thoriumId).Handle, min, max);
-                    }
-
-                    var memoryValues = wmiObject.Get().Cast<ManagementObject>().Select(mo => new
-                    {
-                        FreePhysicalMemory = Double.Parse(mo["FreePhysicalMemory"].ToString()),
-                        TotalVisibleMemorySize = Double.Parse(mo["TotalVisibleMemorySize"].ToString())
-                    }).FirstOrDefault();
-
-                    if (memoryValues != null)
-                    {
-                        var percent = ((memoryValues.TotalVisibleMemorySize - memoryValues.FreePhysicalMemory) / memoryValues.TotalVisibleMemorySize) * 100;
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.WriteLine("Your current memory usage (Thorium): {0}", percent);
-                        Console.ResetColor();
-                    }
-
-                    Thread.Sleep(600);
-                }
-
-                Thread.Sleep(1);
+                Thread.Sleep(5000); // Pausa di 5 secondi per ridurre l'utilizzo della CPU
             }
         }
 
         static void Main(string[] args)
         {
-            Console.Write("Discord and Thorium RAM Limiter - ");
             RamLimiter(-1, -1);
         }
     }
