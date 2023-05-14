03730131de81c2c63b10a7b9541320094d0257c3
NextWork123 <nextworks@protonmail.com>
2023-05-14 11:38:56 +0200

    Refactor the entire code for add Thorium

--- DiscordRamLimiter/Program.cs
+++ DiscordRamLimiter/Program.cs
@@ -4,9 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Management;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace DiscordRamLimiter
 {
@@ -15,67 +13,117 @@ namespace DiscordRamLimiter
         [DllImport("kernel32.dll")]
         static extern bool SetProcessWorkingSetSize(IntPtr proc, int min, int max);
 
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
         public static int GetDiscord()
         {
-            int DiscordId = -1;
+            int discordId = -1;
             long workingSet = 0;
-            foreach(Process discord in Process.GetProcessesByName("Discord"))
+            foreach (Process discord in Process.GetProcessesByName("Discord"))
             {
-                if(discord.WorkingSet64 > workingSet)
+                if (discord.WorkingSet64 > workingSet)
                 {
                     workingSet = discord.WorkingSet64;
-                    DiscordId = discord.Id;
+                    discordId = discord.Id;
                 }
             }
-            return DiscordId;
+            return discordId;
+        }
+
+        public static int GetThorium()
+        {
+            int thoriumId = -1;
+            long workingSet = 0;
+            foreach (Process thorium in Process.GetProcessesByName("Thorium"))
+            {
+                if (thorium.WorkingSet64 > workingSet)
+                {
+                    workingSet = thorium.WorkingSet64;
+                    thoriumId = thorium.Id;
+                }
+            }
+            return thoriumId;
         }
 
         static void RamLimiter(int min, int max)
         {
-            while (GetDiscord() != -1)
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+
+            while (GetDiscord() != -1 || GetThorium() != -1)
             {
                 if (GetDiscord() != -1)
                 {
                     GC.Collect(); // Force garbage collection
+                    GC.WaitForPendingFinalizers(); // Wait for all finalizers to complete before continuing.
 
-                    GC.WaitForPendingFinalizers(); // Wait for all finalizers to complete before continuing. 
-
-                    if (Environment.OSVersion.Platform == PlatformID.Win32NT) // Check OS version platform 
+                    if (Environment.OSVersion.Platform == PlatformID.Win32NT) // Check OS version platform
                     {
                         SetProcessWorkingSetSize(Process.GetProcessById(GetDiscord()).Handle, min, max);
                     }
 
                     var wmiObject = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
-
-                    var memoryValues = wmiObject.Get().Cast<ManagementObject>().Select(mo => new {
+                    var memoryValues = wmiObject.Get().Cast<ManagementObject>().Select(mo => new
+                    {
                         FreePhysicalMemory = Double.Parse(mo["FreePhysicalMemory"].ToString()),
                         TotalVisibleMemorySize = Double.Parse(mo["TotalVisibleMemorySize"].ToString())
                     }).FirstOrDefault();
 
                     if (memoryValues != null)
                     {
-
-
                         var percent = ((memoryValues.TotalVisibleMemorySize - memoryValues.FreePhysicalMemory) / memoryValues.TotalVisibleMemorySize) * 100;
                         Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.WriteLine("Your current memory usage : {0}", percent);
-                        Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Developers: miaf#2458 - faraj#2607");
+                        Console.WriteLine("Your current memory usage (Discord): {0}", percent);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Thread.Sleep(600);
+                    }
+
+                    Thread.Sleep(1);
+                }
+
+                if (GetThorium() != -1)
+                {
+                    GC.Collect(); // Force garbage collection
+                    GC.WaitForPendingFinalizers(); // Wait for all finalizers to complete before continuing.
+
+                    if (Environment.OSVersion.Platform == PlatformID.Win32NT) // Check OS version platform
+                    {
+                        SetProcessWorkingSetSize(Process.GetProcessById(GetThorium()).Handle, min, max);
+                    }
+
+                    var wmiObject = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+                    var memoryValues = wmiObject.Get().Cast<ManagementObject>().Select(mo => new
+                    {
+                        FreePhysicalMemory = Double.Parse(mo["FreePhysicalMemory"].ToString()),
+                        TotalVisibleMemorySize = Double.Parse(mo["TotalVisibleMemorySize"].ToString())
+                    }).FirstOrDefault();
+
+                    if (memoryValues != null)
+                    {
+                        var percent = ((memoryValues.TotalVisibleMemorySize - memoryValues.FreePhysicalMemory) / memoryValues.TotalVisibleMemorySize) * 100;
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Your current memory usage (Thorium): {0}", percent);
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Thread.Sleep(600);
                     }
 
                     Thread.Sleep(1);
                 }
             }
-
         }
 
         static void Main(string[] args)
         {
-            Console.Title = "Discord RAM Limiter by Archwish";
-            Console.Write("Discord RAM Limiter - "); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("https://github.com/miaaf - https://github.com/faraaj");
-
+            Console.Write("Discord and Thorium RAM Limiter - ");
             RamLimiter(-1, -1);
-            
         }
     }
 }
+
