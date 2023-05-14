0562338b7acdf5623e5b05356c27acf777619d31
NextWork123 <nextworks@protonmail.com>
2023-05-14 23:55:55 +0200

    Add ProcessBuffer and improve the perfomance of the code in general

--- DiscordRamLimiter/Program.cs
+++ DiscordRamLimiter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -11,33 +12,29 @@ namespace DiscordRamLimiter
         [DllImport("kernel32.dll")]
         static extern bool SetProcessWorkingSetSize(IntPtr proc, int min, int max);
 
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
-
-        [DllImport("user32.dll")]
-        static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
-
-        const int SW_HIDE = 0;
-        const int SW_SHOW = 5;
+        static string[] targetProcessNames = { "discord", "chrome", "firefox", "edge", "thorium" };
 
         static void RamLimiter(int min, int max)
         {
-            var handle = GetConsoleWindow();
-            ShowWindow(handle, SW_HIDE);
-
-            string[] targetProcessNames = { "discord", "chrome", "firefox", "edge", "thorium" };
+            bool isWindows = Environment.OSVersion.Platform == PlatformID.Win32NT;
+            List<Process> processBuffer = new List<Process>();
 
             while (true)
             {
-                Process[] processes = Process.GetProcesses()
-                    .Where(p => targetProcessNames.Any(name => p.ProcessName.ToLower().Contains(name)))
-                    .ToArray();
-
-                foreach (Process process in processes)
+                if (isWindows)
                 {
-                    if (!process.HasExited && process.Responding && Environment.OSVersion.Platform == PlatformID.Win32NT)
+                    processBuffer.Clear();
+                    processBuffer.AddRange(Process.GetProcesses()
+                        .Where(process =>
+                            targetProcessNames.Any(name => process.ProcessName.IndexOf(name, StringComparison.OrdinalIgnoreCase) >= 0) &&
+                            !process.HasExited && process.Responding));
+
+                    foreach (Process process in processBuffer)
                     {
-                        SetProcessWorkingSetSize(process.Handle, min, max);
+                        if (!process.HasExited)
+                        {
+                            SetProcessWorkingSetSize(process.Handle, min, max);
+                        }
                     }
                 }
 
